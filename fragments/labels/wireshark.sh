0a2bf2ade5fd7fb47db1d725994c2abbe8076e2a
Jared Schwager <jared.schwager@gmail.com>
2025-04-15 09:16:33 -0400

    Added appName variable

--- fragments/labels/wireshark.sh
+++ fragments/labels/wireshark.sh
@@ -7,5 +7,6 @@ wireshark)
       downloadURL=$(curl -fs 'https://www.wireshark.org/download.html' |  xmllint --html --xpath '(//details[1]//a[contains(text(), "macOS Arm Disk Image")]/@href)[1]' - 2>/dev/null |  sed -n 's/.*href="\([^"]*\).*/\1/p')
     fi
     appNewVersion=$(curl -fs 'https://www.wireshark.org/download.html' | grep -oE 'Stable Release: \d(\.\d)+' | grep -oE '\d(\.\d)+' | head -n 1)
+    appName="Wireshark.app"
     expectedTeamID="7Z6EMTD2C6"
     ;;
