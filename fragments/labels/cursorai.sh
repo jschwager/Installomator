4aacfad391214b87ea7031e7babbb03f8b938217
Jared Schwager <jared.schwager@gmail.com>
2026-02-02 16:57:12 -0500

    Update cursorai.sh
    Cursor updated their JSON feed. The stable track no longer offers the most recent version. The latest track now offers the most up to date download.

--- fragments/labels/cursorai.sh
+++ fragments/labels/cursorai.sh
@@ -1,7 +1,7 @@
 cursorai)
     name="Cursor"
     type="dmg"
-    updateFeed=$(curl -fsL  "https://www.cursor.com/api/download?platform=darwin-universal&releaseTrack=stable")
+    updateFeed=$(curl -fsL  "https://www.cursor.com/api/download?platform=darwin-universal&releaseTrack=latest")
     appNewVersion=$(getJSONValue "${updateFeed}" "version")
     downloadURL=$(getJSONValue "${updateFeed}" "downloadUrl")
     expectedTeamID="VDXQ22DGB9"
