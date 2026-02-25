2f46310c188401da5eb476267e5ce1b7286fef64
Jared Schwager <jared.schwager@gmail.com>
2026-02-25 09:58:34 -0500

    Separated Intel/ARC installs
    Added separate appNewVersion and downloadURL for Intel/ARM installations.

--- fragments/labels/tableaudesktop.sh
+++ fragments/labels/tableaudesktop.sh
@@ -2,7 +2,12 @@ tableaudesktop)
     name="Tableau Desktop"
     type="pkgInDmg"
     packageID="com.tableausoftware.tableaudesktop"
-    downloadURL="https://www.tableau.com/downloads/desktop/mac"
-    appNewVersion=${$(curl -fsIL "$downloadURL" | sed -nE 's/.*Desktop-([0-9-]*).*/\1/p')//-/.}
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL="https://www.tableau.com/downloads/desktop/reg-mac-arm64"
+        appNewVersion=${$(curl -fsIL "$downloadURL" | sed -nE 's/.*Desktop-([0-9-]*).*/\1/p' | sed 's/-/./g; s/\.$//')}
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL="https://www.tableau.com/downloads/desktop/mac"
+        appNewVersion=${$(curl -fsIL "$downloadURL" | sed -nE 's/.*Desktop-([0-9-]*).*/\1/p')//-/.}
+    fi
     expectedTeamID="QJ4XPRK37C"
     ;;
